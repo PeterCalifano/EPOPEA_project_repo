784fea0e1e12d27c9c455b879fcebf4150fb2589
paola <paola@10.169.114.125>
2022-10-31 10:46:03 +0100

    i did the first version of the function for propellant mass computation

---
+++ PS/preliminary_prop_mass.m
@@ -0,0 +1,35 @@
+function [mp,m_fu,m_ox] = preliminary_prop_mass(dV,m0)
+% Preliminary propellant mass sizing
+% Computes total propellant mass required for the total dV budget, having
+% preliminary estimated the total dry mass at launch
+% INPUTS
+%       dV = total dV computed (including margins, according to CDF margins
+%       standard) [m/s]
+%       m0[1x1] = total dry mass at launch (including margins, according to
+%       CDF margins standard) [kg]
+%
+% OUTPUTS
+%       mp[1x1] = total propellant mass required for the entire sequence of
+%       maneuvres [kg] (Including 2% margin for propellant residuals)
+%       m_fu [1x1] = fuel mass [kg] according to selected o/f ratio
+%       m_ox [1x1] = oxidizer mass [kg] according to selected o/f ratio
+%
+
+% Constants 
+g0 = 9.81;                                          % [m/s^2]
+% Propellant characteristics
+Is = 300;   % to change                             % [s]
+of = 1.4;   % to change                             
+
+% Apply Tsiolkowsky equation
+mf = m0/exp(dV/(Is*g0));
+mp = m0 - mf;
+% apply margins 
+% from CDF: "the propellant mass shall include 2% for propellant
+% residuals."
+mp = mp*1.02;
+
+m_fu = mp/(1+of);
+m_ox = mp - m_fu;
+
+end

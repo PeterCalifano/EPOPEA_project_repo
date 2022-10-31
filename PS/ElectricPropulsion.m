d8685cce663583ddaf9ab5568e43a2c16fb3a3ff
Matteodambr <96341031+Matteodambr@users.noreply.github.com>
2022-10-31 09:39:42 +0100

    Added margin definition

--- PS/ElectricPropulsion.m
+++ PS/ElectricPropulsion.m
@@ -1,6 +1,16 @@
 clearvars ; close all ; clc ;
 
-
-
+% ------------------------------------------------------------------------------------------------
+%
+% MARGINS TO ADD:
+%   1) Safety factor of 1.5 on total impulse, lifetime, and number of 
+%      cycles of the entire mission
+%
+%   2) Duty cycle: 90% shall be used
+%
+%   3) Volume of propellant tanks: computed with total propellant 
+%      mass + 5% (at least)
+%
+% ------------------------------------------------------------------------------------------------
 
 

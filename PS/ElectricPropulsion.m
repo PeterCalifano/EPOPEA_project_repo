d99b91f4c8688df9e4a3cc53b28ce51f2c562066
Matteodambr <96341031+Matteodambr@users.noreply.github.com>
2022-10-31 09:44:40 +0100

    <electric propulsion> added masses to consider

--- PS/ElectricPropulsion.m
+++ PS/ElectricPropulsion.m
@@ -3,6 +3,7 @@ clearvars ; close all ; clc ;
 % ------------------------------------------------------------------------------------------------
 %
 % MARGINS TO ADD:
+%
 %   1) Safety factor of 1.5 on total impulse, lifetime, and number of 
 %      cycles of the entire mission
 %
@@ -12,5 +13,14 @@ clearvars ; close all ; clc ;
 %      mass + 5% (at least)
 %
 % ------------------------------------------------------------------------------------------------
-
-
+%
+% MASSES TO TAKE INTO ACCOUNT:
+%
+%   1) Electric generator (power processing system)
+%
+%   2) Propellant storage/feed system
+%   
+%   3) Electric thruster assembly
+%
+% ------------------------------------------------------------------------------------------------
+ 

a6a341f5d51a919352a74e888b072fc819db0371
plenghe <paola.grattagliano@mail.polimi.it>
2022-11-05 11:09:30 +0100

    added new script

---
+++ PS/secondary_PS_sizing.m
@@ -0,0 +1,12 @@
+% PS SIZING FOR DIFFERENT MISSION PHASES AND ARCHITECTURES
+
+% Dry mass taken from ROM table without margins (because statistical analysis)
+
+% dV_primary = [ dV_interplanetary, SOI insertion, landing ] -> bipropellant
+% dV_secondary = [ SK ] -> monopropellant
+
+%% SECONDARY SIZING
+% Monopropellant 
+
+%% ORBITER-LANDER
+clear all; close all; clc

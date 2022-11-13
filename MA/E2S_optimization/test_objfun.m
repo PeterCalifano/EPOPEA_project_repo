d3865e394282968b1164f6cf9da3e47f767731a4
peter_califano <petercalifano.gs@gmail.com>
2022-11-13 14:53:01 +0100

    Test with ga solver

--- MA/E2S_optimization/test_objfun.m
+++ MA/E2S_optimization/test_objfun.m
@@ -1,5 +1,25 @@
+close all;
+clear;
+clc;
 
+TU = 365*30*24*3600;
+DU = astroConstants(1);
+R_Saturn = astroConstants(26);
+
+% Fix departure time 
 t1 = 475848/TU;
+
+% Initial guess vector 
 var = [t1, t1+30*3600*24/TU, t1 + 70*3600*24/TU, DU, pi/8, 1000, t1 + 10*3600*24/TU,...
      DU, pi/4, 900, t1 + 50*3600*24/TU];
+
+% Test of the objective function
 DV = objfun_EarthSaturntransfer(var,1,[3,2,3],20*R_Saturn,200*R_Saturn,TU);
+
+% Test with solver
+stoptime = 100;
+opts_ga = optimoptions('ga', 'FunctionTolerance', 1e-12, 'MaxTime', stoptime,...
+    'UseParallel', true, 'PopulationSize', 1000, 'Display', 'iter', 'MaxGenerations', 1e6);
+
+[opt_state, feval, exitflag, output] = ga(@(var) objfun_EarthSaturntransfer(var, 1, [3, 2, 3], 20*R_Saturn,200*R_Saturn,TU),...
+    length(var), [], [], [], [], [], [], [], [], opts_ga);

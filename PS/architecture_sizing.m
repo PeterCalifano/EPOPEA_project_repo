80d99c07181fd8f25948b997aabd9339f42b534c
plenghe <paola.grattagliano@mail.polimi.it>
2022-11-14 21:34:52 +0100

    Refinement of SK division in orbiter / lander

--- PS/architecture_sizing.m
+++ PS/architecture_sizing.m
@@ -16,6 +16,7 @@ tank.sigma = 950e6 ;
 dV_int = 30 + 1.05*0.5e+3 + 1.05*0.9e+3 + 2*1.1e+3 + 10;  % plus MAR-DV-010 / MAR-DV-020 / MAR-DV-080 / MAR-DV-100
 Isp_int = 321; m_dry_int = 1279; % Orbiter + lander
 m_prop_int = preliminary_prop_mass(dV_int,m_dry_int,Isp_int);
+
 %m_prop_int = 3491.9;                    % from preliminary sizing
 
 oxidizer.rho = 1443 ; 
@@ -42,6 +43,7 @@ thruster.chamber_pressure = 10.35e5 ;
 dV_land = 258*2;                            % Additional MAR-DV-020
 Isp_land = 228; m_dry_land = 746;           % Sampling lander
 m_prop_land = preliminary_prop_mass(dV_land,m_dry_land,Isp_land);
+
 %m_prop_land = 95.33;
 
 propellant.mass = m_prop_land ;
@@ -63,9 +65,23 @@ tank.number = 1 ;
 [ sizing_SOSL_LANDING, M_PS_SOSL_LANDING ] = monopropellant_sizing( propellant, pressurant, tank, thruster )
 
 % MONOPROPELLANT - SK - MR106E 22 N
-dV_sk = 370*2;                             % Additional MAR-DV-020
-Isp_sk = 235; m_dry_sk = 533;              % Sampling orbiter
-m_prop_sk = preliminary_prop_mass(dV_sk,m_dry_sk,Isp_sk);
+% Assumption: half of the deltav for SK is used when orbiter and lander are
+% clamped, while the other half is used only by the orbiter.
+
+% Orbiter + lander
+dV_sk = 185*2;                             % Additional MAR-DV-020
+Isp_sk = 235; 
+m_dry_sk_orb_lan = 1279 ;                            % Sampling orbiter
+m_prop_sk_orb_lan = preliminary_prop_mass(dV_sk,m_dry_sk_orb_lan,Isp_sk);
+
+% Only orbiter
+dV_sk = 185*2;                             % Additional MAR-DV-020
+Isp_sk = 235; 
+m_dry_sk_orb = 533;                            % Sampling orbiter
+m_prop_sk_orb = preliminary_prop_mass(dV_sk,m_dry_sk_orb,Isp_sk);
+
+% 
+m_prop_sk = m_prop_sk_orb_lan + m_prop_sk_orb ;
 %m_prop_sk = 205.8;
 
 propellant.mass = m_prop_sk ;
@@ -85,7 +101,6 @@ tank.ratio = 1 ;
 tank.number = 1 ;
 
 [ sizing_SOSL_SK, M_PS_SOSL_SK ] = monopropellant_sizing( propellant, pressurant, tank, thruster )
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -152,9 +167,29 @@ tank.number = 1 ;
 [ sizing_NSOSL_LANDING, M_PS_NSOSL_LANDING ] = monopropellant_sizing( propellant, pressurant, tank, thruster )
 
 % MONOPROPELLANT - SK - MR111C 4 N
-dV_sk = 550*2;                             % Additional MAR-DV-020
-Isp_sk = 229; m_dry_sk = 928;              % NS Orbiter + S Lander (for know, then change)
-m_prop_sk = preliminary_prop_mass(dV_sk,m_dry_sk,Isp_sk);
+% Assumption: half of the deltav for SK is used when orbiter and lander are
+% clamped, while the other half is used only by the orbiter.
+
+% Orbiter + lander
+dV_sk = 275*2;                             % Additional MAR-DV-020
+Isp_sk = 229; 
+m_dry_sk_orb_lan = 928 ;                            % Sampling orbiter
+m_prop_sk_orb_lan = preliminary_prop_mass(dV_sk,m_dry_sk_orb_lan,Isp_sk);
+
+% Only orbiter
+dV_sk = 275*2;                             % Additional MAR-DV-020
+Isp_sk = 229; 
+m_dry_sk_orb = 182;                            % Sampling orbiter
+m_prop_sk_orb = preliminary_prop_mass(dV_sk,m_dry_sk_orb,Isp_sk);
+
+% 
+m_prop_sk = m_prop_sk_orb_lan + m_prop_sk_orb ;
+
+
+
+% dV_sk = 550*2;                             % Additional MAR-DV-020
+% Isp_sk = 229; m_dry_sk = 928;              % NS Orbiter + S Lander (for know, then change)
+% m_prop_sk = preliminary_prop_mass(dV_sk,m_dry_sk,Isp_sk);
 %m_prop_sk = 117.3 ;
 
 propellant.mass = m_prop_sk ;

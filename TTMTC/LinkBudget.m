19aae73f1558222a48967f001cdfb25b3665cd1c
AntoineP-B <antoine.poirierboulet@mail.polimi.it>
2022-11-17 13:11:23 +0100

    Updated version, still a WIP, margins currently obtained are far too small.

--- TTMTC/LinkBudget.m
+++ TTMTC/LinkBudget.m
@@ -13,13 +13,13 @@ min_EbNo_tel = 5.1; % [dB] telemetry case -> BER = 1e-7
 
 % Frequency selection
 f_ka = 32e9; % [Hz] between 27 and 40GHz, for science 
-f_x = 8e9; % between 7 and 10.7 GHz, for interplanetary
+f_x = 8.5e9; % between 7 and 10.7 GHz, for interplanetary
 f_s = 3e9; % between 2 and 4 GHz, for O-L comm
 f_opt = 1e12; %Optical, TBD what frequency is most commonly used
 
 % Antenna data
-D_hga = 2.1; % [m] took from Orbilander paper, page 21 -> WE WOULD NEED 0.165m of diameter to have 3dB margin in uplink
-D_gs = 35; % [m] GS antenna, Cebreros considered (all the same, for ESTRACK DSA)
+D_hga = 3; % [m] took from Orbilander paper, page 21 -> WE WOULD NEED 0.165m of diameter to have 3dB margin in uplink
+D_gs = 70; % [m] GS antenna, Cebreros considered (all the same, for ESTRACK DSA)
 D_lga = 0.065; % [m] from datasheets of existing helix antennas (by RUAG space)
 D_patch = 0.040; % [m] from datasheets of existing patch antenna (by ISIS – Innovative Solutions In Space)
 
@@ -49,7 +49,7 @@ R_up = 700; % [bps] from Orbilander paper, page 133
 
 %% SL + NSO
 R_SLNSO = 81.06*1e3; % bit/s science data downlink
-[EbNo_down_SLNSO, ~] = link_budget('to_Earth', 'downlink', 'atmosphere', R_SLNSO, max_distance_Earth, D_hga, D_gs, f_ka, P_on_board, eta_hga, eta_gs, T_gs);
+[EbNo_down_SLNSO, ~] = link_budget('to_Earth', 'downlink', 'atmosphere', R_SLNSO, max_distance_Earth, D_hga, D_gs, f_x, P_on_board, eta_hga, eta_gs, T_gs);
 [EbNo_up_SLNSO, ~] = link_budget('to_Earth', 'uplink', 'atmosphere', R_up, max_distance_Earth, D_gs, D_hga, f_x, P_gs, eta_gs, eta_hga, T_hga);
 
 disp('SL + NSO')
@@ -58,7 +58,7 @@ marginEbNo_up_SLNSO = EbNo_up_SLNSO - min_EbNo_tel % must be > 3dB
 
 %% SL + SO
 R_SLSO = 82.56*1e3; % bit/s science data downlink
-[EbNo_down_SLSO, ~] = link_budget('to_Earth', 'downlink', 'atmosphere', R_SLSO, max_distance_Earth, D_hga, D_gs, f_ka, P_on_board, eta_hga, eta_gs, T_gs);
+[EbNo_down_SLSO, ~] = link_budget('to_Earth', 'downlink', 'atmosphere', R_SLSO, max_distance_Earth, D_hga, D_gs, f_x, P_on_board, eta_hga, eta_gs, T_gs);
 [EbNo_up_SLSO, ~] = link_budget('to_Earth', 'uplink', 'atmosphere', R_up, max_distance_Earth, D_gs, D_hga, f_x, P_gs, eta_gs, eta_hga, T_hga);
 
 disp('SL + SO')
@@ -70,138 +70,7 @@ R_LtoO = 8.76 * 1e3; % bit/s
 [EbNo_LtoO, ~] = link_budget('L_to_O', 'uplink', 'no_atmosphere', R_LtoO, max_distance_OL, D_lga, D_lga, f_s, P_on_board, eta_lga, eta_lga, T_lga);
 marginEbNo_down_LtoO = EbNo_LtoO - min_EbNo_sci % must be > 3dB
 
-% %% SCIENCE PHASE -> COMMUNICATION MODE, with EARTH
-% 
-% R_down = R_sci * 1.05; % to include also attitude
-% [EbNo_down, SNR_down] = link_budget('to_Earth', 'downlink', 'atmosphere', R_down, max_distance_Earth, D_hga, D_gs, f_ka, P_on_board, eta_hga, eta_gs, T_gs);
-% 
-% marginEbNo_down = EbNo_down - min_EbNo_sci; % must be > 3dB
-% % marginSNR_down = SNR_down - min_SNR; % THIS IS COMMENTED BECAUSE I HAVE
-% % NO IDEA HOW TO RETRIEVE THE MINIMUM SNR
-% 
-% % if UPLINK 
-% R_up = 700; % [bps] from Orbilander paper, page 133
-% [EbNo_up, SNR_up] = link_budget('to_Earth', 'uplink', 'atmosphere', R_up, max_distance_Earth, D_gs, D_hga, f_ka, P_gs, eta_gs, eta_hga, T_hga);
-% 
-% marginEbNo_up = EbNo_up - min_EbNo_tel; % must be > 3dB
-% % marginSNR_up = SNR_up - min_SNR; 
-% 
-% %% SCIENCE PHASE -> COMMUNICATION MODE, with EARTH FROM LANDER
-% 
-% R_down_OL = R_sci_OL * 1.05; % to include also attitude and HK
-% [EbNo_down_OL, SNR_down_OL] = link_budget('to_Earth', 'downlink', 'atmosphere', R_down_OL, max_distance_Earth, D_hga, D_gs, f_ka, P_on_board, eta_hga, eta_gs, T_gs);
-% 
-% marginEbNo_down_OL = EbNo_down_OL - min_EbNo_sci; % must be > 3dB
-% % marginSNR_down_OL = SNR_down_OL - min_SNR; % THIS IS COMMENTED BECAUSE I HAVE
-% % NO IDEA HOW TO RETRIEVE THE MINIMUM SNR
-% 
-% % if UPLINK 
-% R_up_OL = 700*3; % [bps] from Orbilander paper, page 133, TIMES 3 FOR VISIBILITY WINDOW OF LANDER
-% [EbNo_up_OL, SNR_up_OL] = link_budget('to_Earth', 'uplink', 'atmosphere', R_up_OL, max_distance_Earth, D_gs, D_hga, f_ka, P_gs, eta_gs, eta_hga, T_hga);
-% 
-% marginEbNo_up_OL = EbNo_up_OL - min_EbNo_tel; % must be > 3dB
-% % marginSNR_up_OL = SNR_up_OL - min_SNR; 
-% 
-% %% SCIENCE PHASE -> COMMUNICATION BETWEEN ORBITER AND LANDER
-% % FROM LANDER TO ORBITER
-% [EbNo_LtoO, SNR_LtoO] = link_budget('L_to_O', 'uplink', 'no_atmosphere', R_LtoO, max_distance_OL, D_lga, D_lga, f_s, P_on_board, eta_lga, eta_lga, T_lga);
-% 
-% marginEbNo_LtoO = EbNo_LtoO - min_EbNo_sci; % must be > 3dB
-% % marginSNR_down = SNR_down - min_SNR; 
-% 
-% %% SCIENCE PHASE PRINT OUT RESULTS:
-% 
-% fprintf('--- SCIENCE PHASE ---\n')
-% fprintf('ORBITER DEEP SPACE COMMUNICATION CASE\n')
-% fprintf('The Eb/No for Deep Space downlink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_down,marginEbNo_down)
-% fprintf('The Eb/No for Deep Space uplink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_up,marginEbNo_up)
-% fprintf('The Eb/No Orbiter-lander communication is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_LtoO,marginEbNo_LtoO)
-% fprintf('LANDER DEEP SPACE COMMUNICATION CASE\n')
-% fprintf('The Eb/No for Deep Space downlink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_down_OL,marginEbNo_down_OL)
-% fprintf('The Eb/No for Deep Space uplink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_up_OL,marginEbNo_up_OL)
-% 
-% 
-% %% NON-NOMINAL PHASE -> DEEP SPACE COMMUNICATION W/ HGA AT 1/10 POWER
-% % ORBITER DEEP SPACE COMMUNICATION CASE
-% P_NN = 1/10 * P_on_board;
-% %Downlink
-% [EbNo_down_NN_Ploss, SNR_down_NN_Ploss] = link_budget('to_Earth', 'downlink', 'atmosphere', R_NN, max_distance_Earth, D_hga, D_gs, f_ka, P_NN, eta_hga, eta_gs, T_gs);
-% 
-% marginEbNo_down_NN_Ploss = EbNo_down_NN_Ploss - min_EbNo_sci; % must be > 3dB
-% % marginSNR_down_NN_Ploss = SNR_down_NN_Ploss - min_SNR;
-% 
-% %Uplink 
-% [EbNo_up_NN_Ploss, SNR_up_NN_Ploss] = link_budget('to_Earth', 'uplink', 'atmosphere', R_up, max_distance_Earth, D_gs, D_hga, f_ka, P_gs, eta_gs, eta_hga, T_hga);
-% 
-% marginEbNo_up_NN_Ploss = EbNo_up_NN_Ploss - min_EbNo_tel; % must be > 3dB
-% % marginSNR_up_NN_Ploss = SNR_up_NN_Ploss - min_SNR; 
-% 
-% % LANDER DEEP SPACE COMMUNICATION CASE
-% %Downlink
-% [EbNo_down_NN_Ploss_OL, SNR_down_NN_Ploss_OL] = link_budget('to_Earth', 'downlink', 'atmosphere', R_NN_OL, max_distance_Earth, D_hga, D_gs, f_ka, P_NN, eta_hga, eta_gs, T_gs);
-% 
-% marginEbNo_down_NN_Ploss_OL = EbNo_down_NN_Ploss - min_EbNo_sci; % must be > 3dB
-% % marginSNR_down_NN_Ploss_OL = SNR_down_NN_Ploss_OL - min_SNR;
-% 
-% %Uplink 
-% [EbNo_up_NN_Ploss_OL, SNR_up_NN_Ploss_OL] = link_budget('to_Earth', 'uplink', 'atmosphere', R_up_OL, max_distance_Earth, D_gs, D_hga, f_ka, P_gs, eta_gs, eta_hga, T_hga);
-% 
-% marginEbNo_up_NN_Ploss_OL = EbNo_up_NN_Ploss_OL - min_EbNo_tel; % must be > 3dB
-% % marginSNR_up_NN_Ploss = SNR_up_NN_Ploss_OL - min_SNR; 
-% 
-% fprintf('\n\n--- NON-NOMINAL PHASE ---\n')
-% fprintf('\n- CASE : 1/10 POWER HGA DEEP SPACE COMM.\n')
-% fprintf('ORBITER DEEP SPACE COMMUNICATION CASE\n')
-% fprintf('The Eb/No for Deep Space downlink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_down_NN_Ploss,marginEbNo_down_NN_Ploss)
-% fprintf('The Eb/No for Deep Space uplink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_up_NN_Ploss,marginEbNo_up_NN_Ploss)
-% fprintf('LANDER DEEP SPACE COMMUNICATION CASE\n')
-% fprintf('The Eb/No for Deep Space downlink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_down_NN_Ploss_OL,marginEbNo_down_NN_Ploss_OL)
-% fprintf('The Eb/No for Deep Space uplink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_up_NN_Ploss_OL,marginEbNo_up_NN_Ploss_OL)
-% 
-% %% NON-NOMINAL PHASE -> DEEP SPACE COMMUNICATION W/ LGA 
-% % ORBITER DEEP SPACE COMMUNICATION CASE
-% %Downlink
-% [EbNo_down_NN_LGA, SNR_down_NN_LGA] = link_budget('to_Earth', 'downlink', 'atmosphere', R_NN, max_distance_Earth, D_lga, D_gs, f_ka, P_on_board, eta_lga, eta_gs, T_gs);
-% 
-% marginEbNo_down_NN_LGA = EbNo_down_NN_LGA - min_EbNo_sci; % must be > 3dB
-% % marginSNR_down_NN_LGA = SNR_down_NN_LGA - min_SNR;
-% 
-% %Uplink 
-% [EbNo_up_NN_LGA, SNR_up_NN_LGA] = link_budget('to_Earth', 'uplink', 'atmosphere', R_up, max_distance_Earth, D_gs, D_lga, f_ka, P_gs, eta_gs, eta_lga, T_lga);
-% 
-% marginEbNo_up_NN_LGA = EbNo_up_NN_LGA - min_EbNo_tel; % must be > 3dB
-% % marginSNR_up_NN_LGA = SNR_up_NN_LGA - min_SNR; 
-% 
-% % LANDER DEEP SPACE COMMUNICATION CASE
-% %Downlink
-% [EbNo_down_NN_LGA_OL, SNR_down_NN_LGA_OL] = link_budget('to_Earth', 'downlink', 'atmosphere', R_NN_OL, max_distance_Earth, D_lga, D_gs, f_ka, P_on_board, eta_lga, eta_gs, T_gs);
-% 
-% marginEbNo_down_NN_LGA_OL = EbNo_down_NN_LGA - min_EbNo_sci; % must be > 3dB
-% % marginSNR_down_NN_LGA_OL = SNR_down_NN_LGA_OL - min_SNR;
-% 
-% %Uplink 
-% [EbNo_up_NN_LGA_OL, SNR_up_NN_LGA_OL] = link_budget('to_Earth', 'uplink', 'atmosphere', R_up_OL, max_distance_Earth, D_gs, D_lga, f_ka, P_gs, eta_gs, eta_lga, T_lga);
-% 
-% marginEbNo_up_NN_LGA_OL = EbNo_up_NN_LGA_OL - min_EbNo_tel; % must be > 3dB
-% % marginSNR_up_NN_Ploss = SNR_up_NN_LGA_OL - min_SNR; 
-% 
-% fprintf('\n- CASE : LGA DEEP SPACE COMM.\n')
-% fprintf('ORBITER DEEP SPACE COMMUNICATION CASE\n')
-% fprintf('The Eb/No for Deep Space downlink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_down_NN_LGA,marginEbNo_down_NN_LGA)
-% fprintf('The Eb/No for Deep Space uplink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_up_NN_LGA,marginEbNo_up_NN_LGA)
-% fprintf('LANDER DEEP SPACE COMMUNICATION CASE\n')
-% fprintf('The Eb/No for Deep Space downlink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_down_NN_LGA_OL,marginEbNo_down_NN_LGA_OL)
-% fprintf('The Eb/No for Deep Space uplink is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_up_NN_LGA_OL,marginEbNo_up_NN_LGA_OL)
-% 
-% %% NON-NOMINAL PHASE -> ORBITER-LANDER COMMUNICATION W/ PATCH 
-% % FROM LANDER TO ORBITER
-% [EbNo_LtoO_NN, SNR_LtoO_NN] = link_budget('L_to_O', 'uplink', 'no_atmosphere', R_LtoO, max_distance_OL, D_patch, D_lga, f_s, P_on_board, eta_lga, eta_lga, T_lga);
-% 
-% marginEbNo_LtoO_NN = EbNo_LtoO_NN - min_EbNo_sci; % must be > 3dB
-% % marginSNR_LtoO_NN = SNR_LtoO_NN - min_SNR; 
-% fprintf('\n- CASE : PATCH COMM FOR O-L\n')
-% fprintf('The Eb/No Orbiter-lander communication is: %.2f \nIts corresponding margin is: %.2f\n',EbNo_LtoO_NN,marginEbNo_LtoO_NN)
-% 
+
 
 %% FUNCTION
 function [EbNo, SNR] = link_budget(condition, direction, atmosphere, R, max_distance, Dtx, Drx, f, Pinput, eta_tx, eta_rx, Ts)
@@ -242,19 +111,22 @@ if strcmp(condition, 'to_Earth')
         La = 0; % [dB] if no atmospheric losses are considere (L-O comm)
     end
 
-    % Reciever gain
-    Grx = 10*log10(pi*Drx^2*eta_rx/lambda^2);
+    % Reciever gain %TO BE CHECKED WITH GS DATASHEET
+    Grx = 61.5;%10*log10(pi*Drx^2*eta_rx/lambda^2);
 
     % Compute beamwidth (parabolic reflector case)
-    theta_tx = 65.3*lambda/Dtx;
-    theta_rx = 65.3*lambda/Drx;
+    theta_tx = 65.3*lambda/Dtx; % [deg]
+    theta_rx = 65.3*lambda/Drx; % [deg]
 
     % Pointing losses <- comes from the pointing budget!
-    err = 0.1/100; % typical value 0.1% as from the slides
-    Lp = -12*(err/theta_rx)^2;
+    err = 0.01; % typical value 0.1 deg as from the slides
+    Lp = -12*(err/theta_tx)^2;
 
     % Power recieved
     Prx = EIRP + Ls + La + Grx + Lp;
+
+    %Total losses
+    Losses = Ls + Lc + La + Lp;
 
     % Reciever noise density (due to temperature)
     k = 1.38e-23; % Boltzmann's constant

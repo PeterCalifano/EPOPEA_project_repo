67ad7e537d54258c84fc293b41da96e50796af16
plenghe <paola.grattagliano@mail.polimi.it>
2022-11-17 14:19:31 +0100

    Corrected mistake on the dV, added more margins from standard, added a margin chosen by us on landing, added maneuvering time computation

--- PS/architecture_sizing_DM.m
+++ PS/architecture_sizing_DM.m
@@ -20,9 +20,20 @@ tank.sigma = 950e6 ;
 mdot_reg = 0.135;
 mdot_blow = 0.0035;
 %%%%%%%%
+n_flyby = 4;             % Number of flybys
+
+% Margins
+MAR_010 = 1.05;          % Deterministic maneuvers
+MAR_020 = 2;             % Stochastic
+MAR_050 = 1.2;           % Apply on LANDING
+MAR_030 = 2;             % Attitude maneuvres
+MAR_080 = 30;            % [m/s] Launcher dispersion - apply only on primary
+MAR_090 = 15*n_flyby;    % [m/s] For each flyby
+MAR_100 = 10;            % [m/s] For Moon approach navigation maneuvre
+MAR_hazard = 80;         % [kg] Sum to LANDING propellant mass
 
 % PRESSURE REGULATED: ARIANE 400N BI-PROPELLANT APOGEE THRUSTER
-dV_reg = 30 + 1.05*0.5e+3 + 1.05*0.9e+3 + 2*1.1e+3 + 10;  % plus MAR-DV-010 / MAR-DV-020 / MAR-DV-080 / MAR-DV-100
+dV_int = MAR_080 + MAR_020*0.5e+3 + MAR_010*0.9e+3 + MAR_020*1.1e+3 + MAR_100 + MAR_090;  % plus MAR-DV-010 / MAR-DV-020 / MAR-DV-080 / MAR-DV-100
 Isp_reg = 321; m_dry_int = 1279;                          % Orbiter + lander
 m_prop_reg = preliminary_prop_mass(dV_reg,m_dry_int,Isp_reg);
 
@@ -39,12 +50,12 @@ dt_eoi = m_prop_eoi / mdot_reg ;
 % Assumption: half of the deltav for SK is used when orbiter and lander are
 % clamped, while the other half is used only by the orbiter.
 % Orbiter + lander
-dV_blow = 185*2;                             % Additional MAR-DV-020
+dV_blow = 185*MAR_020;                             % Additional MAR-DV-020
 Isp_blow = 292; 
 m_dry_sk_orb_lan = 1279 ;                    % Sampling orbiter
 m_prop_sk_orb_lan = preliminary_prop_mass(dV_blow,m_dry_sk_orb_lan,Isp_blow);
 % Only orbiter
-dV_blow = 185*2;                             % Additional MAR-DV-020
+dV_blow = 185*MAR_020;                             % Additional MAR-DV-020
 Isp_blow = 292; 
 m_dry_sk_orb = 533;                          % Sampling orbiter
 m_prop_sk_orb = preliminary_prop_mass(dV_blow,m_dry_sk_orb,Isp_blow);
@@ -103,13 +114,13 @@ dt_eoi = m_prop_eoi / mdot_reg ;
 % Assumption: half of the deltav for SK is used when orbiter and lander are
 % clamped, while the other half is used only by the orbiter.
 % Orbiter + lander
-dV_sk = 275*2;                             % Additional MAR-DV-020
+dV_sk = 275*MAR_020;                             % Additional MAR-DV-020
 Isp_sk = 292; 
 m_dry_sk_orb_lan = 928 ;                            % Sampling orbiter
 m_prop_sk_orb_lan = preliminary_prop_mass(dV_sk,m_dry_sk_orb_lan,Isp_sk);
 
 % Only orbiter
-dV_sk = 275*2;                             % Additional MAR-DV-020
+dV_sk = 275*MAR_020;                             % Additional MAR-DV-020
 Isp_sk = 292; 
 m_dry_sk_orb = 182;                            % Sampling orbiter
 m_prop_sk_orb = preliminary_prop_mass(dV_sk,m_dry_sk_orb,Isp_sk);

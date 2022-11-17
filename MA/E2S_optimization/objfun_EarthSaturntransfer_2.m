cc92341d6eca28888d849639f5a9b7a0c51b59d4
mlusv <m.lusvarghi@gmail.com>
2022-11-17 15:35:50 +0100

    1) modified the main script to check the obj fun 2) implemented the adimensionalization in the obj fun

--- MA/E2S_optimization/objfun_EarthSaturntransfer_2.m
+++ MA/E2S_optimization/objfun_EarthSaturntransfer_2.m
@@ -10,7 +10,7 @@ function DV = objfun_EarthSaturntransfer_2(nlpvar, planets_id, Ra_target, Rp_tar
 % planets_id - String of identifier of planets visited (dep and arrival included)
 % Ra_target - Apoapsis Radius for capture orbit (SEE EstimateDVtoCapture.m)
 % Rp_target - Periapsis Radius for capture orbit (SEE EstimateDVtoCapture.m)
-% TU - Time Unit used to adimensionalize (It's considered to be 1 year)
+% TU - Time Unit used to adimensionalize from seconds to days 
 % -------------------------------------------------------------------------------------------------------------
 %% OUTPUT
 % DV: [scalar] Sum of the DSMs and of the DVs given during the Powered Fly Bys
@@ -20,7 +20,19 @@ function DV = objfun_EarthSaturntransfer_2(nlpvar, planets_id, Ra_target, Rp_tar
 % -------------------------------------------------------------------------------------------------------------
 %% EXPLANATION OF VAR VECTOR
 %
-% var = 
+% var = [
+%       t_1 --> initial time [DAYS]
+%       v_inf --> magnitude of the departure infinite velocity [Km/s]
+%       u,v --> parameters related to RA and Dec at departure [ ]
+%       ToF_1, ..., ToF_(N+1) --> ToF for each arc from 2 planets [DAYS]
+%       alpha_1, ..., alpha_(N+1) --> adimensional number from 0 to 1 that
+%           indicated the percentage of ToF_i at which the DSM is performed
+%           (e.g. if alpha_2 = 0.5, the 2nd DSM is performed at ToF_2/2 of
+%           the second arc)
+%       rp_1, ... , rp_N --> Radii of pericenter for each FB, normalized by
+%           the mean radius of the FB planet [Planetary Radii]
+%       beta_1, ... , beta_N --> angle of hyperbolic plane of each FB [rad]
+%       ];
 % -------------------------------------------------------------------------------------------------------------
 %% DEPENDENCIES
 % 
@@ -44,13 +56,18 @@ function DV = objfun_EarthSaturntransfer_2(nlpvar, planets_id, Ra_target, Rp_tar
 %% CONSTANTS DEFINITION
 mu_S = astroConstants(4);
 
+DU = astroConstants(2);
+TU = 365.5;
+TU2 = TU*3600*24;
+VU = DU/TU2;
+
 % Obtain the number of fly-bys from the string of visited planets
 N = (length(nlpvar) - 6)/4;
 
 %% EXTRACT VARIABLES FROM VAR
 % Static allocation
 t1 = nlpvar(1);
-ephtimes = zeros(N+2,1); % ephemeris time in [s]
+ephtimes = zeros(N+2,1); % ephemeris time in JULIAN DAYS
 ephtimes(1) = t1;
 
 
@@ -77,18 +94,20 @@ beta_FB = nlpvar((3*N+7):(3*N+6 + N));
 %% Retrieve positions of the planets
 r_planets = zeros(3,N+2);
 v_planets = zeros(3,N+2);
+Rad_planets = zeros(1,N+2);
+mu_planets = zeros(1,N+2);
 
 for i = 1:(N+2)
 
-    % Obtain the time in days for uplanet
-    jd_i = ephtimes(i) / (24 * 3600);
-
     % Retrieve keplerian coordinates and convert them
-    [kep1,~] = uplanet(jd_i, planets_id(i));
+    [kep1,~] = uplanet(ephtimes(i), planets_id(i));
     [r1,v1] = kep2car(kep1, mu_S);
     r_planets(:,i) = r1;
     v_planets(:,i) = v1;
 
+    % Retrieve mean radius of each planet [Km]
+    Rad_planets(i) = astroConstants(20 + planets_id(i));
+    mu_planets(i) = astroConstants(10 + planets_id(i));
 end
 
 %% DEPARTURE 
@@ -103,7 +122,6 @@ phi = acos(2*v-1)-pi/2;
 % Departure v_inf in local frame
 v_inf_dep_local = v_inf_dep*[cos(th)*cos(phi);sin(th)*cos(phi);sin(phi)]';
 
-
 % Build rotation matrix
 i1 = v_E / norm(v_E);
 k1 = cross(r_E,v_E)/norm(cross(r_E,v_E));
@@ -114,6 +132,19 @@ R = [i1,j1,k1];
 v_inf_dep = R*v_inf_dep_local';
 
 v_dep = v_inf_dep + v_E;
+
+%% ADIMENSIONALIZATION
+ephtimes = ephtimes/TU;
+tof = tof/TU;
+rp_FB = (rp_FB .* Rad_planets(2:end-1)) / DU;
+v_dep = v_dep / VU;
+r_planets = r_planets / DU;
+v_planets = v_planets / VU;
+mu_S = mu_S * TU2^2 / DU^3;
+mu_planets = mu_planets * TU2^2 / DU^3;
+Ra_target = Ra_target / DU;
+Rp_target = Rp_target / DU;
+
 
 %% Compute Transfer Arcs
 
@@ -139,7 +170,7 @@ for i = 1:N+1
     % Propagation from initial state until the DSM
     %x_DSM = propagate(x_0,[ephtimes(i),ephtimes(i) + tof_1]);
 
-    [r_propagated, v_propagated, ~] = PropagatorHelio_2BP(x_0, tof_1,mu_S);
+    [r_propagated, v_propagated, ~] = PropagatorHelio_2BP(x_0, tof_1, mu_S);
 
     r_DSM(:,i) = r_propagated(:,end);
     v_DSM(:,i,1) = v_propagated(:,end);
@@ -154,8 +185,7 @@ for i = 1:N+1
     if i ~= (N+1)
         
         % Find gravitational constant of the planet
-        mu_pl = astroConstants(20 + planets_id(i+1));
-
+        mu_pl = mu_planets(i+1);
 
         % Obtain the incoming infinite velocity
         v_inf_in = v_FB_in - v_planets(:,i+1);
@@ -175,7 +205,7 @@ end
 
 %% Compute CAPTURE DV: 
 
-mu_end = astroConstants(20 + planets_id(end)); % CAN BE MOVED OUT THE OBJ FUNCTION AND PASSED TO IT
+mu_end = mu_planets(end);
 
 Vinf_entry = v_FB_in - v_planets(:,end);
 
@@ -185,15 +215,15 @@ norm_Vinf_entry = norm(Vinf_entry);
 
 %% COMPUTE DV:
 
-
 DV_DSM = zeros(1,N+1);
+
 for i = 1:(N+1)
 
-    DV_DSM(i) = norm(v_DSM(i,:,2) - v_DSM(i,:,1));
+    DV_DSM(i) = norm(v_DSM(:,i,2) - v_DSM(:,i,1));
 
 end
 
-DV = DV_capture + sum(DV_DSM);
+DV = (DV_capture + sum(DV_DSM)) * VU;
 
 
 end

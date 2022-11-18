ee37d279c8d126728d7cad23c81be27b9202f510
elenapilo <elena.pilo@mail.polimi.it>
2022-11-18 08:42:34 +0100

    Added dyn funciton to simulate initial orbit. Corrected small typos in Landing

---
+++ MA/Landing/dyn.m
@@ -0,0 +1,16 @@
+function [dxdt] = dyn (~, state, par)
+% UNFOLD
+% state: r, v
+r = state(1:2);
+v = state(3:4);
+
+% par: mu
+mu = par(4);
+
+
+
+% dynamics 
+dxdt = [v; 
+        -mu/(norm(r)^3).*r];
+
+end

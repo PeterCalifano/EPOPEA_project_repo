c9bb9adc46838d356efa3ea34697fc0de4785cf2
peter_califano <petercalifano.gs@gmail.com>
2022-11-18 15:10:20 +0100

    Update main script GTO

--- MA/E2S_optimization/GTO_MGA1DSM_ClusterPruning.m
+++ MA/E2S_optimization/GTO_MGA1DSM_ClusterPruning.m
@@ -12,24 +12,27 @@ cspice_furnsh('..\..\spice_kernels/de440s.bsp')
 
 %% Problem initialization
 % Define algorithm parameters
-N1 = 100; % n° Global optimization runs per iter (how many tentative solution are found by ga)
+N1 = 10; % n° Global optimization runs per iter (how many tentative solution are found by ga)
 iter = 1; 
 maxiter = 20; % Number of maximum allowed iteration of while loop
 perc = 80; % Percentile of objective function for LB, UB update
 cost_thr = 1; % DV cost in km/s ?
-stoptime = 45; % Stop time for ga solver
-maxtime =  10.5*3600; % Max allowable execution time
+stoptime = 4; % Stop time for ga solver
+maxtime =  2*3600; % Max allowable execution time
 
 rng shuffle
 
 % GA options
 opts_ga = optimoptions('ga', 'FunctionTolerance', 1e-10, 'MaxTime', stoptime,...
-    'UseParallel', true, 'PopulationSize', 250, 'Display', 'iter', 'MaxGenerations', 1e3,...
-    'CrossoverFraction', 0.8, 'MaxStallGenerations', 10);
+    'UseParallel', true, 'PopulationSize', 100, 'Display', 'iter', 'MaxGenerations', 1e3,...
+    'CrossoverFraction', 0.7, 'MaxStallGenerations', 3);
 % Fminunc options
 opts_fmincon = optimoptions('fmincon', 'Display', 'iter', 'FunctionTolerance', 1e-12,...
-               'OptimalityTolerance', 1e-9, 'MaxFunctionEvaluations', 2e5, 'MaxIterations', 300, ...
+               'OptimalityTolerance', 1e-9, 'MaxFunctionEvaluations', 2e5, 'MaxIterations', 10, ...
                'StepTolerance', 1e-12);
+
+opts_simulanneal = optimoptions('simulannealbnd', 'Display', 'iter',...
+    'FunctionTolerance', 1e-10, 'MaxTime', stoptime, 'MaxIterations', 400);
 
 % Number of flybys
 N_fb = 3;
@@ -44,13 +47,20 @@ Rp_target = 3*astroConstants(26);
 % Static allocation
 LB = zeros(maxiter, length(NLPvars));
 UB = zeros(maxiter, length(NLPvars));
+
 NLPoptset_ga = zeros(N1, length(NLPvars), maxiter);
 feval_ga = zeros(N1, 1, maxiter);
 exitflag_ga = zeros(N1, 1, maxiter);
 
+NLPoptset_simulanneal = zeros(N1, length(NLPvars), maxiter);
+feval_simulanneal = zeros(N1, 1, maxiter);
+exitflag_simulanneal = zeros(N1, 1, maxiter);
+
 NLPoptset_local = zeros(N1, length(NLPvars), maxiter);
 feval_local = zeros(N1, 1, maxiter);
 exitflag_local = zeros(N1, 1, maxiter);
+
+
 
 % Define INITIAL [LB, UB] for all decision variables
 % LB_benchmark = [];
@@ -84,11 +94,16 @@ UB(1, :) = [UBt_launchdate, UBvinfdep, UBu, UBv, UBtof, UBeta, UBRp_seq, UBbeta]
 converge_flag = 0;
 
 time_limit = tic();
+% codegen -config:mex -lang::c++ -report ...
+%        -args {N1, iter, NLPvars, planet_seq, Ra_target, Rp_target, LB, UB, params, NLPoptset_ga, feval_ga, exitflag_ga} ...
+%        -nargout {1} HeuristicOptimization
 
 while converge_flag ~= 1 
 
-    % DEVNOTE: CONVERT HEUR AND LOCAL IN MEX
 
+    % DEVNOTE: CONVERT HEUR AND LOCAL IN MEX
+%     output = HeuristicOptimization(N1, iter, NLPvars, planet_seq, Ra_target, Rp_target, LB, UB, opts_ga, NLPoptset_ga, feval_ga, exitflag_ga);
+   
     for idsol = 1:N1
         % Heuristic optimization: produce f_ga costs [N1x1] and corresponding
         % decision variables NLPoptset_ga(idsol, :, iter)
@@ -97,8 +112,16 @@ while converge_flag ~= 1
             length(NLPvars), [], [], [], [], LB(iter, :), UB(iter, :), [], opts_ga);
     end
 
+%      for idsol = 1:N1
+%         % Heuristic optimization: produce f_ga costs [N1x1] and corresponding
+%         % decision variables NLPoptset_ga(idsol, :, iter)
+%         [NLPoptset_simulanneal(idsol, :, iter), feval_simulanneal(idsol, 1, iter), exitflag_simulanneal(idsol, 1, iter)] =...
+%             simulannealbnd(@(NLPvars) objfun_EarthSaturntransfer_2(NLPvars, planet_seq, Ra_target, Rp_target),...
+%             NLPvars, LB(iter, :), UB(iter, :), opts_simulanneal);
+%     end
+
     parfor idsol = 1:N1
-        % Local optimization (refinement): produce f_local costs [N1x1] and 
+        % Local optimization (refinement): produce f_local costs [N1x1] and
         % corresponding decision variables NLPoptset_local(idsol, :, iter)
         [NLPoptset_local(idsol, :, iter), feval_local(idsol, 1, iter), exitflag_local(idsol, 1, iter)] = ...
             fmincon(@(NLPvars) objfun_EarthSaturntransfer_2(NLPvars, planet_seq, Ra_target, Rp_target),...
@@ -111,46 +134,58 @@ while converge_flag ~= 1
             converge_flag = 1;
         end
 
-        % Redefine LB for iter+1
-        LB_min = min(NLPoptset_local(:, :, iter), [], 1); % Find minimum of decision variables
 
-        LB_new = LB_min - (UB(iter, :) - LB(iter, :))*2*perc/100;
+        % Find percentile of cost
+       perc_value = prctile(feval_local(:, :, iter), perc);
+       feval_temp = feval_local(:, :, iter);
+       % Create mask to extract only solutions below desired percentile
+       maskid_perc = feval_temp < perc_value;
+       % Extract decision vectors corresponding to these solutions
+       NLPopts_perc = NLPoptset_local(maskid_perc == 1, :, iter);
+       % Find minimum of decision variables perc percentile
+       LB_min = min(NLPopts_perc, [], 1);
+       % Find maximum of decision variables perc percentile
+       UB_max = max(NLPopts_perc, [], 1);
 
-        % Redefine UB for iter+1
-        UB_max = max(NLPoptset_local(:, :, iter), [], 1); % Find maximum of decision variables
-        UB_new = UB_max + (UB(iter, :) - LB(iter, :))*2*perc/100;
+       % Redefine LB for iter+1
+%        LB_min = min(NLPoptset_local(:, :, iter), [], 1); % Find minimum of decision variables
+       LB_new = LB_min - (UB(iter, :) - LB(iter, :))*2*perc/100;
 
-        for idVar = 1:length(NLPvars)
+       % Redefine UB for iter+1
+%        UB_max = max(NLPoptset_local(:, :, iter), [], 1); % Find maximum of decision variables
+       UB_new = UB_max + (UB(iter, :) - LB(iter, :))*2*perc/100;
 
-            if LB_new(idVar) > LB(iter, idVar)
-                % If new LB is within previous LB
-                LB(iter+1, :) = LB_min - (UB(iter, :) - LB(iter, :))*2*perc/100;
-            else
-                % Assign previous LB
-                LB(iter+1, idVar) = LB(iter, idVar);
-            end
+       for idVar = 1:length(NLPvars)
+
+           if LB_new(idVar) > LB(iter, idVar)
+               % If new LB is within previous LB
+               LB(iter+1, :) = LB_min - (UB(iter, :) - LB(iter, :))*2*perc/100;
+           else
+               % Assign previous LB
+               LB(iter+1, idVar) = LB(iter, idVar);
+           end
 
 
-            if UB_new(idVar) < UB(iter, idVar)
-                % If new UB is within previous UB
-                UB(iter+1, idVar) = UB_max + (UB(iter, :) - LB(iter, :))*2*perc/100;
-                else
-                    % Assign previous UB
-                    UB(iter+1, idVar) = UB(iter, idVar);
-            end
-        end
+           if UB_new(idVar) < UB(iter, idVar)
+               % If new UB is within previous UB
+               UB(iter+1, idVar) = UB_max + (UB(iter, :) - LB(iter, :))*2*perc/100;
+           else
+               % Assign previous UB
+               UB(iter+1, idVar) = UB(iter, idVar);
+           end
+       end
     end
 
     % Check if allowed maxiter reached
     elapsedtime = toc(time_limit);
 
-    if iter == maxiter 
+    if iter == maxiter
         break;
     end
 
     % Update iteration counter
     iter = iter + 1;
-   
+
     % Check if maxtime reached
     if  elapsedtime >= maxtime
         break;

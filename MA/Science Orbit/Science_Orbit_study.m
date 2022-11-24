f67c261532328ca4042d7b976087daf60e0c09fb
FabrizioMaccari <117080604+FabrizioMaccari@users.noreply.github.com>
2022-11-24 23:28:25 +0100

    Plot carini

--- MA/Science Orbit/Science_Orbit_study.m
+++ MA/Science Orbit/Science_Orbit_study.m
@@ -17,6 +17,36 @@ R_enc=252.1; %km, Enceladus mean radius
 
 R_enc=R_enc/DU;
 
+% L1 determination
+%Scalar potential function and gradient definition
+%Symbolic manipulation
+syms x y z
+r1_sym= ((x+mu)^2+y^2+z^2).^0.5;
+r2_sym= ((x+mu-1)^2+y^2+z^2).^0.5;
+
+U_sym=@(x) 1/2*(x.^2+y.^2) + (1-mu)./r1_sym + mu./r2_sym+1/2*mu*(1-mu);
+%dU/dx of the scalar potential function 
+Ux_sym=diff(U_sym,x);
+
+
+%Function handle recovery
+Ux=matlabFunction(Ux_sym);
+
+U_der_vec_x=@(x) Ux(x(1),0,0);
+
+%zero finding
+options_fzero=optimset('Display','iter','TolX',1e-15,'TolFun',1e-15);
+format long
+
+U_der_vec_x_fzero=@(x) Ux(x,0,0);
+x00=1.1; %initial guess
+x_L2_fzero=fzero(U_der_vec_x_fzero,x00,options_fzero)
+x_L2=[x_L2_fzero;0;0];
+
+
+
+
+
 %CRTBP dynamics, function handle
 r1=@(t,x) ((x(1)+mu).^2+x(2).^2+x(3).^2).^0.5;
 r2=@(t,x) ((x(1)+mu-1).^2+x(2).^2+x(3).^2).^0.5;
@@ -40,9 +70,9 @@ vz0_Halo=0;
 state0_Halo=[x0_Halo,y0_Halo,z0_Halo,vx0_Halo,vy0_Halo,vz0_Halo]';
 
 t0=0;
-%FlightDays=2; %days of prapagation
-%tf=FlightDays*24*3600/TU; %final time of propagation
-tf=1*3600/TU;  
+FlightDays=10; %days of prapagation
+tf=FlightDays*24*3600/TU; %final time of propagation
+ 
 
 
 %propagation - Halo
@@ -77,10 +107,49 @@ state_vec_P=state_vec_P';
 
 
 Enceladus_3D(R_enc*DU,[(1-mu)*DU,0,0])
-plot3(state_vec_Halo(1,:),state_vec_Halo(2,:),state_vec_Halo(3,:),'k')
-%plot3(state_vec_P(1,:)*DU,state_vec_P(2,:)*DU,state_vec_P(3,:)*DU,'b')
+P2=plot3(state_vec_Halo(1,:),state_vec_Halo(2,:),state_vec_Halo(3,:),'k','linewidth',1.25);
+P1=plot3(x_L2(1)*DU,x_L2(2)*DU,x_L2(3)*DU,'ob','markersize',5,'linewidth',1.25);
+grid minor
+legend([P1 P2],'$L_2$','Southern Halo orbit')
 
-% %% ground tracks
+
+Enceladus_3D(R_enc*DU,[(1-mu)*DU,0,0])
+P1=plot3(state_vec_P(1,:)*DU,state_vec_P(2,:)*DU,state_vec_P(3,:)*DU,'b','linewidth',1);
+grid minor
+legend(P1,'Periodic orbit')
+
+%% Science orbit subdivision
+% propagation - half remote sensing arc
+tf_RS=0.75*3600/TU;
+[t_vec_RS,state_vec_RS]=ode113(@(t,x) f(t,x,mu),[t0 tf_RS],state0_Halo,options_ode);
+state_vec_RS=state_vec_RS';
+state_vec_RS(1:3,:)=state_vec_RS(1:3,:)*DU;
+state_vec_RS(4:6,:)=state_vec_RS(4:6,:)*DU/TU;
+
+%propagation - everything else/2
+state0_else=[state_vec_RS(1:3,end)/DU;state_vec_RS(4:6,end)*TU/DU];
+
+options_ode_event = odeset( 'RelTol', 1e-13, 'AbsTol', 1e-13,'Events',@(t,x) FirstZeroCrossing(t,x));
+
+[t_vec_else,state_vec_else,t_e,state_e,i_e]=ode113(@(t,x) f(t,x,mu),[tf_RS tf],state0_else,options_ode_event);
+state_vec_else=state_vec_else';
+state_vec_else(1:3,:)=state_vec_else(1:3,:)*DU;
+state_vec_else(4:6,:)=state_vec_else(4:6,:)*DU/TU;
+
+
+Enceladus_3D(R_enc*DU,[(1-mu)*DU,0,0])
+P1=plot3(state_vec_RS(1,:),state_vec_RS(2,:),state_vec_RS(3,:),'b','linewidth',1.25);
+P2=plot3(state_vec_else(1,:),state_vec_else(2,:),state_vec_else(3,:),'r','linewidth',1.25);
+plot3(state_vec_RS(1,:),-state_vec_RS(2,:),state_vec_RS(3,:),'b','linewidth',1.25);
+plot3(state_vec_else(1,:),-state_vec_else(2,:),state_vec_else(3,:),'r','linewidth',1.25);
+grid minor
+legend([P1 P2],'Remote sensing arc','Communication and analysis arc')
+
+
+
+
+
+%% Ground Tracks (TBA)
 % w_Enc=1/TU;
 % 
 % 
@@ -118,6 +187,16 @@ plot3(state_vec_Halo(1,:),state_vec_Halo(2,:),state_vec_Halo(3,:),'k')
 % grid on
 % grid minor
 
+
+%% event function
+function [value,isterminal,direction] = FirstZeroCrossing(t,x) %Event function 
+%to stop the integration at the intersection point with the x-z plane 
+
+value=x(2);
+isterminal=1;
+direction=-1;
+
+end
 
 
 

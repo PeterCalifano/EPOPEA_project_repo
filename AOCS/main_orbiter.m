c0dd4178a27d40828604d9f11d6207dfffebef39
elenapilo <elena.pilo@mail.polimi.it>
2022-12-09 12:51:07 +0100

    Added rotation of halo state in simulink block

--- AOCS/main_orbiter.m
+++ AOCS/main_orbiter.m
@@ -7,6 +7,9 @@ rng shuffle
 J_NSO = diag([6277.21, 10241.83, 12845.78]); % [kg m^2]
 J_SO = diag([6961.96, 16022.69, 18620.98]); % [kg m^2]
 
+muS = 3.7931187*1e16;            %[m^3/s^2]
+muE = (6.6743e-11)*(1.0802e20);  %[m^3/s^2]
+
 % Map: 1) NSO, 2) SO, 3) SL
 model = 1;
 
@@ -26,16 +29,12 @@ wb0 = 0.01*rand(3, 1);
 qrand = rand(4, 1);
 qbn0 = qrand./norm(qrand);
 
-t0 = 0;
-tf = 1000;
-tspan = t0:0.1:tf;
-
 % Science Orbit
-state_str = load('Halo_10days.mat');
-state = cell2mat(struct2cell(state_str))';
-st_pos = state(:,1:3);
+state_str = load('Halo_3days.mat');
+state = cell2mat(struct2cell(state_str));
+st_pos = state(:,:);
 L = length(st_pos(:,1));
-Xpos = reshape(st_pos', [3,1,L]);
+Xpos = reshape(st_pos', [6,1,L]);
 
 period = 12*3600;
 

c1b6925b6844a7fec4f9c82f4444f30a7f6f4698
peter_califano <petercalifano.gs@gmail.com>
2022-12-09 21:53:43 +0100

    Bug fixing Simulink model

--- AOCS/main_orbiter.m
+++ AOCS/main_orbiter.m
@@ -2,13 +2,21 @@ close all
 clear
 clc
 
+DefaultFontSize = 16;
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'defaultLegendInterpreter', 'latex');
+set(groot, 'defaulttextinterpreter', 'latex');
+set(0, 'defaultAxesFontSize', DefaultFontSize)
+
 rng shuffle
+
 %% Model parameters
 J_NSO = diag([6277.21, 10241.83, 12845.78]); % [kg m^2]
 J_SO = diag([6961.96, 16022.69, 18620.98]); % [kg m^2]
 
 muS = 3.7931187*1e16;            %[m^3/s^2]
 muE = (6.6743e-11)*(1.0802e20);  %[m^3/s^2]
+
 
 % Map: 1) NSO, 2) SO, 3) SL
 model = 1;
@@ -25,18 +33,29 @@ switch model
 end
 
 %% Initial conditions
-wb0 = 0.01*rand(3, 1);
+wb0 = 0.00001*rand(3, 1);
 qrand = rand(4, 1);
 qbn0 = qrand./norm(qrand);
 
-% Science Orbit
-state_str = load('Halo_3days.mat');
-state = cell2mat(struct2cell(state_str));
-st_pos = state(:,:);
-L = length(st_pos(:,1));
-Xpos = reshape(st_pos', [6,1,L]);
+t0 = 0;
+tf = 1000;
+tspan = t0:0.1:tf;
 
-period = 12*3600;
+
+
+% Science Orbit
+n_days = 3;
+filename = "Halo_" + num2str(n_days) + "days.mat";
+state_str = load('Halo_3days.mat');
+state = cell2mat(struct2cell(state_str))';
+st_pos = state;
+L = length(st_pos);
+
+Xpos = reshape(st_pos', [6, 1, L]);
+
+StopTime = n_days*24*3600;
+
+% period = 12*3600;
 
 % % Gyros model parameters
 % % Random Noise Generation - Gyros
@@ -55,5 +74,6 @@ period = 12*3600;
 % S = eye(3);
 
 
-%out = sim(ModelName, 'Timeout', tf);
+out = sim(ModelName, 'Timeout', StopTime);
+
 

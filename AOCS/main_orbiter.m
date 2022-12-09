0fa267e9991537abfa256cf72e148958499fc95c
elenapilo <elena.pilo@mail.polimi.it>
2022-12-09 11:23:19 +0100

    Commented sensors. Changed Inertias. Added Halo state

--- AOCS/main_orbiter.m
+++ AOCS/main_orbiter.m
@@ -4,8 +4,8 @@ clc
 
 rng shuffle
 %% Model parameters
-J_NSO = diag([13760.61, 15399.52, 15621.60]); % [kg m^2]
-J_SO = diag([16903.03, 17415.25, 24963.03]); % [kg m^2]
+J_NSO = diag([6277.21, 10241.83, 12845.78]); % [kg m^2]
+J_SO = diag([6961.96, 16022.69, 18620.98]); % [kg m^2]
 
 % Map: 1) NSO, 2) SO, 3) SL
 model = 1;
@@ -30,22 +30,30 @@ t0 = 0;
 tf = 1000;
 tspan = t0:0.1:tf;
 
-% Gyros model parameters
+% Science Orbit
+state_str = load('Halo_10days.mat');
+state = cell2mat(struct2cell(state_str))';
+st_pos = state(:,1:3);
+Xpos = reshape(st_pos', [3,1,4423]);
 
-% Random Noise Generation - Gyros
-Nsamples = length(tspan);
+period = 12*3600;
 
-sigma_RRW = 0.001;
-sigma_ARW = 0.001;
+% % Gyros model parameters
+% % Random Noise Generation - Gyros
+% Nsamples = length(tspan);
+% 
+% sigma_RRW = 0.001;
+% sigma_ARW = 0.001;
+% 
+% R_RRW = diag(ones(1, 3)*sigma_RRW^2);
+% R_ARW = diag(ones(1, 3)*sigma_ARW^2);
+% 
+% RRW_noise = mvnrnd(zeros(3, 1), R_RRW, Nsamples);
+% ARW_noise = mvnrnd(zeros(3, 1), R_ARW, Nsamples);
+% 
+% % Misalignment
+% S = eye(3);
 
-R_RRW = diag(ones(1, 3)*sigma_RRW^2);
-R_ARW = diag(ones(1, 3)*sigma_ARW^2);
 
-RRW_noise = mvnrnd(zeros(3, 1), R_RRW, Nsamples);
-ARW_noise = mvnrnd(zeros(3, 1), R_ARW, Nsamples);
-
-% Misalignment
-S = eye(3);
-
-out = sim(ModelName, 'Timeout', tf);
+%out = sim(ModelName, 'Timeout', tf);
 

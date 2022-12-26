fd96473f6e5e76d6187f2568a35a0683d7212930
alecream <103047493+alecream@users.noreply.github.com>
2022-12-26 15:03:30 +0100

    TCS: main file for orbiter created

---
+++ TCS/TCS_Orbiter.m
@@ -0,0 +1,6 @@
+%% Set Latex font
+set(groot,'defaulttextinterpreter','latex')
+set(groot,'defaultLegendInterpreter','latex')
+set(groot,'defaultAxesTickLabelInterpreter','latex')
+%%
+clearvars; clc; close all

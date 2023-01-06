249b713f27e654fe219f4770d2c4387dd56bef54
mlusv <m.lusvarghi@gmail.com>
2023-01-06 12:32:51 +0100

    Simple Shooting update

--- MA/Science Orbit/Station Keeping/nlcon_SK.m
+++ MA/Science Orbit/Station Keeping/nlcon_SK.m
@@ -33,7 +33,7 @@ Gc_eq = [];
 
 % Propagate 
 options_ode = odeset( 'RelTol', 1e-13, 'AbsTol', 1e-13);
-[~,prop_state] = ode113(@SCR3BP_dyn,[t0, t0 + (tf-t0)/2, tf],xx_0,options_ode,mu_tbp,mu_v,R_v,J2_v);
+[~,prop_state] = ode113(@SCR3BP_dyn,[t0, tf],xx_0,options_ode,mu_tbp,mu_v,R_v,J2_v);
 xx_f = prop_state(end,:)';
 
 % Compute the inequality constraints
@@ -41,10 +41,31 @@ c(1) = norm(xx_0(1:3) - r_nom_i) - threshold_r;
 c(2) = norm(xx_f(1:3) - r_nom_f) - threshold_r;
 
 if flag ~= 0
-    
-    r_apse = prop_state(2,1:3)';
+    if flag == 2
+        max_dist = 0;
+        for i = 1:length(prop_state(1,:))
+            dist = norm(prop_state(i,1:3));
+            if dist > max_dist
+                max_dist = dist;
+                index_apse = i;
+            end
+        end
+        
+    elseif flag == 1
+        min_dist = 1e+10;
+        for i = 1:length(prop_state(1,:))
+            dist = norm(prop_state(i,1:3));
+            if dist < min_dist
+                min_dist = dist;
+                index_apse = i;
+            end
+        end
+
+    end
+    r_apse = prop_state(index_apse,1:3) - [1-mu_tbp,0,0];
     c(3) = - norm(r_apse) + bounds(1);
     c(4) = norm(r_apse) - bounds(2);
-
+    a = 1;
 end
+
 end

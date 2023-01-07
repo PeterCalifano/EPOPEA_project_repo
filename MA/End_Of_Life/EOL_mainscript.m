411a98254fb442939c7a69817b1c4c84a962bb77
mlusv <m.lusvarghi@gmail.com>
2023-01-07 12:23:12 +0100

    Grand Finale has begun

---
+++ MA/End_Of_Life/EOL_mainscript.m
@@ -0,0 +1,50 @@
+%%%%%%%%%%%%%%%%%%%%% END OF LIFE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Load SPICE Kernels
+cspice_kclear();
+try
+    cspice_furnsh('spice_kernels/pck00010.tpc')
+    cspice_furnsh('spice_kernels/naif0012.tls')
+    cspice_furnsh('spice_kernels/gm_de431.tpc')
+    cspice_furnsh('spice_kernels/de440s.bsp')
+    cspice_furnsh('spice_kernels/sat441.bsp')
+catch
+    cspice_furnsh('..\..\spice_kernels/pck00010.tpc')
+    cspice_furnsh('..\..\spice_kernels/naif0012.tls')
+    cspice_furnsh('..\..\spice_kernels/gm_de431.tpc')
+    cspice_furnsh('..\..\spice_kernels/de440s.bsp')
+end
+
+%% DATA
+clear; close all; clc;
+
+% Initial Halo State
+x0_Halo=1.000062853735440;
+y0_Halo=0;
+z0_Halo=-0.00117884381145460;
+vx0_Halo=0;
+vy0_Halo=0.0168877463349484;
+vz0_Halo=0;
+x0_Halo=[x0_Halo,y0_Halo,z0_Halo,vx0_Halo,vy0_Halo,vz0_Halo]';
+
+% Design variables
+% var = [x_1, t_1, t_f];
+
+% Set the bounds
+A = [];
+B = [];
+Aeq = [];
+Beq = [];
+ub = [];
+lb = [];
+
+
+
+
+
+
+
+
+
+
+
+

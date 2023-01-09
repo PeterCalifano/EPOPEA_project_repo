51e1fe9baa108b805cf10f7e476e15b128bd71a5
mlusv <m.lusvarghi@gmail.com>
2023-01-09 16:24:36 +0100

    DSN Coverage is done (To be tested for Enceladus because my PC is stupid with spice, but it works with the Sun)

--- MA/DSN_Coverage.m
+++ MA/DSN_Coverage.m
@@ -2,7 +2,7 @@
 clearvars; clc; cspice_kclear();
 
 % Define Kernel
-cd('..')
+%cd('..')
 cspice_furnsh('spice_kernels/naif0012.tls')
 cspice_furnsh('spice_kernels/de440s.bsp')
 cspice_furnsh('spice_kernels/sat441.bsp')
@@ -19,26 +19,32 @@ cspice_furnsh('spice_kernels\earthstns_itrf93_201023.bsp')
 cspice_furnsh('spice_kernels\earth_fixed.tf')
 cspice_furnsh('spice_kernels\earth_latest_high_prec.bpc')
 cspice_furnsh('spice_kernels\earth_topo_201023.tf')
+cspice_furnsh('spice_kernels\earth_200101_990628_predict.bpc')
 cd('MA')
 %% Preliminary operations
-
+clear; clc;
 % Define constants
 mu_Sun=cspice_bodvrd('SUN','GM',1);
 
 % Create vector with the times of interest
+
+%%%%%%%%% SET AT NEED OF THE USER %%%%%%%%%%%%%%%
 et0 = cspice_str2et('2047 AUG 27 21:19:55 UTC');
-etf = cspice_str2et('2055 AUG 27 21:19:55 UTC');
-et_vec = linspace(et0,etf,1000);
+etf = cspice_str2et('2047 SEP 29 21:19:55 UTC');
+delta_et = 3600; 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+et_vec = et0:delta_et:etf;
 
 % Names of the stations
-stations = {'DSS-14','DSS-43','DSS_63'};
+stations = {'DSS-14','DSS-43','DSS-63'};
 
 %% Compute position of Enceladus in ECI
 
 Earth2Enc = zeros(6,length(et_vec));
 
 for ii = 1:length(et_vec)
-    Earth2Enc(:,ii) = cspice_spkezr('Sun', et_vec(ii), 'ECLIPJ2000', 'NONE', 'Earth');
+    Earth2Enc(:,ii) = cspice_spkezr('602', et_vec(ii), 'ECLIPJ2000', 'NONE', 'Earth');
 end
 
 %% Compute the elevations of Saturn System
@@ -59,8 +65,25 @@ for id = 1:length(stations)
         [R, Az, El] = measurement_model(stations{id},t_i,rv_eci);
 
         % Save Azimuth and Elevation
-        elevations(id,i) = El;
+        elevations(id,i) = El*cspice_dpr();
     end
 end
-%%
-ROT_ECI2TOPO = cspice_sxform('J2000', 'DSS-13_TOPO' , et0);
+
+%% Plot the elevations for each station
+for id = 1:3
+    figure
+    plot(et_vec/cspice_spd(), elevations(id,:),'b')
+    grid minor
+    hold on
+    xlabel('Time since SOI [days]')
+    ylabel('Elevation [deg]')
+    title(stations{id})
+end
+
+
+
+
+
+
+
+

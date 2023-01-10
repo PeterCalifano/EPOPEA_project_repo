3fc9c6365a33e6b39d997c5eefa1766e5c07af10
elenapilo <elena.pilo@mail.polimi.it>
2023-01-10 11:13:55 +0100

    changed sensors and actuators

--- AOCS/main_orbiter.m
+++ AOCS/main_orbiter.m
@@ -1,6 +1,4 @@
-close all
-clear
-
+clearvars; close all; clc
 
 DefaultFontSize = 16;
 set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
@@ -11,24 +9,21 @@ set(0, 'defaultAxesFontSize', DefaultFontSize)
 rng shuffle
 
 %% Model parameters
-J_NSO = diag([6597.09, 10138.44, 13254.21]); % [kg m^2]
-J_SO = diag([6846.3, 15903.78, 18425.41]); % [kg m^2]
+J_SO = diag([12043.36, 15990.19, 8547.84]); % [kg m^2]
 
 muS = 3.7931187*1e16;            %[m^3/s^2]
 muE = (6.6743e-11)*(1.0802e20);  %[m^3/s^2]
 
+% Geometry
+inc = deg2rad(45);  % Thruster inclination wrt face 
+lx = 3.600; % [m]
+ly = 2.500; % [m]
+lz = 3.610; % [m]
+D = 15*1e-2; % [m] Z offset wrt CM
 
-% Map: 1) NSO+SL, 2) SO+SL
-model = 2;
+J = J_SO;
+ModelName = 'ADCS_orbiter';  
 
-switch model
-    case 1
-        J = J_NSO;
-        ModelName = 'ADCS_orbiter';
-    case 2
-        J = J_SO;
-        ModelName = 'ADCS_orbiter';  
-end
 
 %% Initial conditions
 wb0 = 0.001*rand(3, 1);
@@ -50,28 +45,46 @@ L = length(st_pos);
 Xpos = reshape(st_pos, [6, 1, L]);
 
 StopTime = n_days*24*3600;
-
 period = 12*3600;
 
-% % Gyros model parameters
-% % Random Noise Generation - Gyros
+%% Sensors
+% Gyros model parameters
 % Nsamples = length(tspan);
-% 
-% sigma_RRW = 0.001;
-% sigma_ARW = 0.001;
-% 
+sigma_RRW = 0.001;
+sigma_ARW = 0.001;
 % R_RRW = diag(ones(1, 3)*sigma_RRW^2);
 % R_ARW = diag(ones(1, 3)*sigma_ARW^2);
-% 
 % RRW_noise = mvnrnd(zeros(3, 1), R_RRW, Nsamples);
 % ARW_noise = mvnrnd(zeros(3, 1), R_ARW, Nsamples);
-% 
-% % Misalignment
-% S = eye(3);
 
+%% Actuators
+% 8 actuators 
 
-out = sim(ModelName, 'Timeout', StopTime);
+% MR 103J 1N Rocket Engine Thrusters
+% MIB = 0.0133 Ns
+F_min = 0.19;  % N
+F_max = 1.13; % N
+MIB_real=F_min*0.1;
 
+MIB_max=0.05;
+Dt_min=MIB_max/F_min;
+
+R = 0.5*[-ly*cos(inc)              ly*cos(inc)            ly*cos(inc)            -ly*cos(inc)             -ly*cos(inc)               ly*cos(inc)              ly*cos(inc)             -ly*cos(inc)          
+          D*sin(inc)+lx*cos(inc)   D*sin(inc)-lx*cos(inc) D*sin(inc)+lx*cos(inc)  D*sin(inc)-lx*cos(inc)  -D*sin(inc)-lx*cos(inc)   -D*sin(inc)+lx*cos(inc)  -D*sin(inc)-lx*cos(inc)  -D*sin(inc)+lx*cos(inc)   
+         -ly*sin(inc)             -ly*sin(inc)            ly*sin(inc)             ly*sin(inc)              ly*sin(inc)               ly*sin(inc)             -ly*sin(inc)             -ly*sin(inc)             ];
+
+w = ones(length(R(1,:)),1);
+
+R_ast = pinv(R);
+
+%% Control 
+k_p = 0;
+k_d = 100;
+w_c = [0 0 0]';
+
+%% Run Simulink
+% out = sim(ModelName, 'Timeout', StopTime);
+return
 %% Post-processing
 set(groot,'defaultAxesTickLabelInterpreter','latex');  
 set(groot,'defaultLegendInterpreter','latex');
@@ -108,7 +121,8 @@ legend('$T_{x}$', '$T_{y}$', '$T_{z}$');
 % Maximum torque of GG from simulation
 max_gg = max(out.T_GG_norm);
 fprintf('\nPeak Gravity Gradient Torque along 6 orbits:\n\t-%f', max_gg);
-%% Momentum Storage
+
+% Momentum Storage
 
 tin = out.tout(1);
 tf = out.tout(end);
